5c876cf492c7e165c65b7efca0474d7e378b7cdc
Mayr08 <mayr97.sm@gmx.at>
2021-12-03 10:24:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,21 @@
 from mycroft import MycroftSkill, intent_file_handler
+# For NeoPixels on Raspberry Pi
+import time
+import board
+import neopixel
 
+
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D21
+# The number of NeoPixels
+num_pixels = 24
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
+)
 
 class UniversityOfHagenberg(MycroftSkill):
     def __init__(self):
