f98698f911666160db992b7d5bf530cb4e1ef036
Mayr08 <mayr97.sm@gmx.at>
2021-12-03 11:09:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ import time
 import board
 import neopixel
 
+import RPi.GPIO as GPIO
+LED = 21
 
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D21
@@ -17,13 +19,21 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
 )
 
+
 class UniversityOfHagenberg(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         
     def _turnOnLED(self):
-        pixels.fill((255, 0, 0))
-        pixels.show()
+        try:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(LED, GPIO.OUT)
+            GPIO.output(LED, GPIO.HIGH)
+        except GPIO.error:
+            self.log.warning("Can't initialize GPIO - skill will not load")
+        #pixels.fill((255, 0, 0))
+        #pixels.show()
         time.sleep(5)
         
     @intent_file_handler('hagenberg.of.university.intent')
